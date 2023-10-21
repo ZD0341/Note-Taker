8e4119004632529c8b458df6910735f6050268f3
ZD0341 <144489565+ZD0341@users.noreply.github.com>
2023-10-20 17:18:11 -0700

    got it working

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-//:   app.use('/api', api);     <- should we add this??
-
 //serve asset library from this folder
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf-8', (err, data) => {
@@ -26,14 +27,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 function getNotes(callback) {
-  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      const notes = JSON.parse(data);
-      callback(null, notes);
-    }
-  });
+
 }
 
 // app.post('/api/notes', (req, res) => {
@@ -48,26 +42,35 @@ function getNotes(callback) {
 app.post('/api/notes', (req, res) => {
   // Assuming req.body contains the new note data in JSON format
   const newNote = req.body;
-  console.log(req.body)
-  const notes = getNotes();
-  console.log(notes)
-  
-
-  var count = Object.keys(notes).length;
-  console.log(count);
-  newNote.id = count + 1; //using index as id
-
-  // Add the new note to the existing notes array
-  notes.push(newNote);
-
-  // Write the updated notes back to the JSON file
-  fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
+  //console.log(req.body)
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
     if (err) {
-      res.status(500).send(`Error writing the JSON file\n${err}`);
+      console.error(err);
     } else {
-      res.json(newNote);
+      const notes = JSON.parse(data);
+      //console.log(notes)
+
+      var count = Object.keys(notes).length;
+      //console.log(count);
+      newNote.id = count + 1; //using index as id
+
+      // Add the new note to the existing notes array
+      notes.push(newNote);
+
+      // Write the updated notes back to the JSON file
+      fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
+        if (err) {
+          res.status(500).send(`Error writing the JSON file\n${err}`);
+        } else {
+          res.json(newNote);
+        }
+      });
+
     }
   });
+
+
+
 });
 
 
